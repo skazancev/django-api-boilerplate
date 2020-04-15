7e2f8cd8fba1babbac0cfd60a079961ccb2e2dcf
skazancev <stas.kazancev54@gmail.com>
2020-04-15 23:33:24 +0600

    init

---
+++ docker/service.web.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /code/
+echo $(ls)
+
+
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
+
+gunicorn main.wsgi:application -b 0.0.0.0:8000 --reload --worker-connections=1000 --workers=5 --threads=3
